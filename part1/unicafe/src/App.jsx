73a676a3fe0d4d21c8572fcc5df5c23a6de7bfda
Marco Bacchetta <marcobacchetta95@gmail.com>
2026-08-01 19:29:01 -0300

    Ex 1.6 Unicafe, first step

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const Button = ({ text, handleClick }) => (
+    <button onClick={handleClick}>{text}</button>
+  )
+
+  return (
+    <div>
+      <h2>Give Feedback ^_^</h2>
+      <Button text="good" handleClick={handleGoodClick} />
+      <Button text="neutral" handleClick={handleNeutralClick} />
+      <Button text="bad" handleClick={handleBadClick} />
+      <br />
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
