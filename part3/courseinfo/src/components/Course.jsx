25172d97444be5b274faed097be58394727fd4dc
Marco Bacchetta <marcobacchetta95@gmail.com>
2026-08-04 01:17:29 -0300

    Ex. 2.4 and 2.5, steps 9 and 10

--- part3/courseinfo/src/components/Course.jsx
+++ part3/courseinfo/src/components/Course.jsx
@@ -3,12 +3,18 @@ import Content from "./Content"
 import Total from "./Total"
 
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
         <div>
-            <Header title={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            {courses.map(course => {
+                console.log('course', course)
+                return (
+                    <div key={course.id}>
+                        <Header title={course.name} />
+                        <Content parts={course.parts} />
+                        <Total parts={course.parts} />
+                    </div>)
+            })}
         </div>
     )
 }
